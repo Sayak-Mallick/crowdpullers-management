65443c6ad6811fc7c09b3ec2e70800e4f39c1416
Sayak-Mallick <sayakmallickkv@gmail.com>
2026-03-10 06:04:36 +0530

    [TASK] : added user registration api integration

--- frontend/src/components/signup-form.tsx
+++ frontend/src/components/signup-form.tsx
@@ -1,21 +1,59 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
+} from "@/components/ui/card";
 import {
   Field,
   FieldDescription,
   FieldGroup,
   FieldLabel,
-} from "@/components/ui/field"
-import { Input } from "@/components/ui/input"
-import { Link } from "react-router-dom"
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { register } from "@/services/api/auth.endpoints";
+import { useMutation } from "@tanstack/react-query";
+import { LoaderCircle } from "lucide-react";
+import { useRef } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export function SignupForm({ ...props }: React.ComponentProps<typeof Card>) {
+  const navigate = useNavigate();
+  const nameRef = useRef<HTMLInputElement>(null);
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+  const confirmPasswordRef = useRef<HTMLInputElement>(null);
+
+  const mutation = useMutation({
+    mutationFn: register,
+    onSuccess: () => {
+      navigate("/auth/login");
+    },
+  });
+
+  const handleRegister = () => {
+    const name = nameRef.current?.value;
+    const email = emailRef.current?.value;
+    const password = passwordRef.current?.value;
+    const confirmPassword = confirmPasswordRef.current?.value;
+
+    if (!name || !email || !password || !confirmPassword) {
+      return;
+    }
+
+    if (password.length < 8) {
+      return;
+    }
+
+    if (password !== confirmPassword) {
+      return;
+    }
+
+    mutation.mutate({ name, email, password });
+  };
+
   return (
     <Card {...props}>
       <CardHeader>
@@ -23,30 +61,38 @@ export function SignupForm({ ...props }: React.ComponentProps<typeof Card>) {
         <CardDescription>
           Enter your information below to create your account
         </CardDescription>
+        {mutation.isError && (
+          <p className="text-sm text-red-500 mt-1">
+            {mutation.error.message || "Something went wrong"}
+          </p>
+        )}
       </CardHeader>
       <CardContent>
         <form>
           <FieldGroup>
             <Field>
               <FieldLabel htmlFor="name">Full Name</FieldLabel>
-              <Input id="name" type="text" placeholder="John Doe" required />
+              <Input
+                id="name"
+                ref={nameRef}
+                type="text"
+                placeholder="Enter name"
+                required
+              />
             </Field>
             <Field>
               <FieldLabel htmlFor="email">Email</FieldLabel>
               <Input
                 id="email"
                 type="email"
-                placeholder="m@example.com"
+                ref={emailRef}
+                placeholder="Enter email"
                 required
               />
-              <FieldDescription>
-                We&apos;ll use this to contact you. We will not share your email
-                with anyone else.
-              </FieldDescription>
             </Field>
             <Field>
               <FieldLabel htmlFor="password">Password</FieldLabel>
-              <Input id="password" type="password" required />
+              <Input id="password" ref={passwordRef} type="password" required />
               <FieldDescription>
                 Must be at least 8 characters long.
               </FieldDescription>
@@ -55,12 +101,27 @@ export function SignupForm({ ...props }: React.ComponentProps<typeof Card>) {
               <FieldLabel htmlFor="confirm-password">
                 Confirm Password
               </FieldLabel>
-              <Input id="confirm-password" type="password" required />
+              <Input
+                id="confirm-password"
+                ref={confirmPasswordRef}
+                type="password"
+                required
+              />
               <FieldDescription>Please confirm your password.</FieldDescription>
             </Field>
             <FieldGroup>
               <Field>
-                <Button type="submit">Create Account</Button>
+                <Button
+                  type="button"
+                  className="w-full flex items-center gap-1"
+                  onClick={handleRegister}
+                  disabled={mutation.isPending}
+                >
+                  {mutation.isPending && (
+                    <LoaderCircle className="animate-spin" />
+                  )}
+                  <span>Sign Up</span>
+                </Button>
                 {/*<Button variant="outline" type="button">
                   Sign up with Google
                 </Button>*/}
@@ -73,5 +134,5 @@ export function SignupForm({ ...props }: React.ComponentProps<typeof Card>) {
         </form>
       </CardContent>
     </Card>
-  )
+  );
 }
--- frontend/src/services/api/auth.endpoints.ts
+++ frontend/src/services/api/auth.endpoints.ts
@@ -1,5 +1,9 @@
 import { api } from "../wrapper/fetch.wrapper";
 
+export const register = async (data: { name: string; email: string; password: string }) => {
+  return api.post("/api/users/register", data);
+};
+
 export const login = async (data: { email: string; password: string }) => {
   return api.post("/api/users/login", data);
 };
