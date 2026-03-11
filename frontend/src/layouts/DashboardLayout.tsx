9d295f66c9dac8fb4bee517da46620bb1f98354a
Sayak-Mallick <sayakmallickkv@gmail.com>
2026-03-11 22:18:45 +0530

    [TASK] : set up api and table for clients

--- frontend/src/layouts/DashboardLayout.tsx
+++ frontend/src/layouts/DashboardLayout.tsx
@@ -47,7 +47,7 @@ const DashboardLayout = () => {
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
       <div className="hidden border-r bg-muted/40 md:block">
         <div className="flex h-full max-h-screen flex-col gap-2">
-          <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
+          <div className="flex h-14 items-center border-b px-4 lg:h-15 lg:px-6">
             <Link to="/" className="flex items-center gap-2 font-semibold">
               <Package2 className="h-6 w-6" />
               <span className="">Crowd Pullers</span>
@@ -103,7 +103,7 @@ const DashboardLayout = () => {
         </div>
       </div>
       <div className="flex flex-col">
-        <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
+        <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-15 lg:px-6">
           <Sheet>
             <SheetTrigger asChild>
               <Button
--- frontend/src/pages/ClientPage.tsx
+++ frontend/src/pages/ClientPage.tsx
@@ -1,6 +1,134 @@
+import { Badge } from "@/components/ui/badge";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { getClients } from "@/services/api/clients.endpoints";
+import { useQuery } from "@tanstack/react-query";
+import { MoreHorizontal } from "lucide-react";
+import ReactSVG from "../assets/react.svg"
+
 const ClientPage = () => {
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["clients"],
+    queryFn: getClients,
+  });
+
   return (
-    <div>BooksPage</div>
+    <>
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/">Home</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>Clients</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+      <Card className="mt-6">
+        <CardHeader>
+          <CardTitle>Clients</CardTitle>
+          <CardDescription>
+            Manage your clients and their details
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="hidden w-25 sm:table-cell">
+                  <span className="sr-only">Image</span>
+                </TableHead>
+                <TableHead>Title</TableHead>
+                <TableHead>Genre</TableHead>
+                <TableHead className="hidden md:table-cell">
+                  Author name
+                </TableHead>
+                <TableHead className="hidden md:table-cell">
+                  Created at
+                </TableHead>
+                <TableHead>
+                  <span className="sr-only">Actions</span>
+                </TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              <TableRow>
+                <TableCell className="hidden sm:table-cell">
+                  <img
+                    alt={ReactSVG}
+                    className="aspect-square rounded-md object-cover"
+                    height="64"
+                    src={ReactSVG}
+                    width="64"
+                  />
+                </TableCell>
+                <TableCell className="font-medium">title</TableCell>
+                <TableCell>
+                  <Badge variant="outline">genre</Badge>
+                </TableCell>
+                <TableCell className="hidden md:table-cell">
+                  author.name
+                </TableCell>
+                <TableCell className="hidden md:table-cell">
+                  book.createdAt
+                </TableCell>
+                <TableCell>
+                  <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                      <Button aria-haspopup="true" size="icon" variant="ghost">
+                        <MoreHorizontal className="h-4 w-4" />
+                        <span className="sr-only">Toggle menu</span>
+                      </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                      <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                      <DropdownMenuItem>Edit</DropdownMenuItem>
+                      <DropdownMenuItem>Delete</DropdownMenuItem>
+                    </DropdownMenuContent>
+                  </DropdownMenu>
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </CardContent>
+        <CardFooter>
+          <div className="text-xs text-muted-foreground">
+            Showing <strong>1-10</strong> of <strong>32</strong> products
+          </div>
+        </CardFooter>
+      </Card>
+    </>
   );
 };
 
