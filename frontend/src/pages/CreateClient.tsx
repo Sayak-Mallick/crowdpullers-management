c69e5e90e6976913ccd8a42a8c888dd18deb1d93
Sayak-Mallick <sayakmallickkv@gmail.com>
2026-04-15 10:54:44 +0530

    [TASK] : add client completed

--- frontend/src/pages/CreateClient.tsx
+++ frontend/src/pages/CreateClient.tsx
@@ -1,5 +1,154 @@
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useForm } from "react-hook-form";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { LoaderCircle } from "lucide-react";
+import { Link, useNavigate } from "react-router-dom";
+import { createClient } from "@/services/api/clients.endpoints";
+
+const formSchema = z.object({
+  title: z.string().min(3, {
+    message: "Title must be at least 3 characters.",
+  }),
+  clientLogo: z.instanceof(FileList).refine((file) => {
+    return file.length == 1;
+  }, "Client Logo is required"),
+});
+
 const CreateClient = () => {
-  return <div>CreateClient</div>;
+  const navigate = useNavigate();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+    },
+  });
+
+  const clientLogoRef = form.register("clientLogo");
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation({
+    mutationFn: createClient,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["clients"] });
+      console.log("Client created successfully");
+      navigate("/clients");
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    const formdata = new FormData();
+    formdata.append("name", values.title);
+    formdata.append("clientLogo", values.clientLogo[0]);
+    mutation.mutate(formdata);
+  }
+
+  return (
+    <section>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <div className="flex items-center justify-between">
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem>
+                  <Link to="/">Home</Link>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator />
+                <BreadcrumbItem>
+                  <Link to="/clients">Clients</Link>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator />
+                <BreadcrumbItem>
+                  <BreadcrumbPage>Create</BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+            <div className="flex items-center gap-4">
+              <Link to="/clients">
+                <Button variant={"outline"}>
+                  <span className="ml-2">Cancel</span>
+                </Button>
+              </Link>
+              <Button type="submit" disabled={mutation.isPending}>
+                {mutation.isPending && (
+                  <LoaderCircle className="animate-spin" />
+                )}
+                <span className="ml-2">Submit</span>
+              </Button>
+            </div>
+          </div>
+          <Card className="mt-6">
+            <CardHeader>
+              <CardTitle>Create a client</CardTitle>
+              <CardDescription>
+                Fill out the form below to create a new client.
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <div className="grid gap-6">
+                <FormField
+                  control={form.control}
+                  name="title"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Title</FormLabel>
+                      <FormControl>
+                        <Input type="text" className="w-full" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="clientLogo"
+                  render={() => (
+                    <FormItem>
+                      <FormLabel>Client Logo</FormLabel>
+                      <FormControl>
+                        <Input
+                          type="file"
+                          className="w-full"
+                          {...clientLogoRef}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+            </CardContent>
+          </Card>
+        </form>
+      </Form>
+    </section>
+  );
 };
 
 export default CreateClient;
--- frontend/src/services/api/clients.endpoints.ts
+++ frontend/src/services/api/clients.endpoints.ts
@@ -1,5 +1,22 @@
+import useTokenStore from "@/store";
 import { api } from "../wrapper/fetch.wrapper";
+
+api.interceptors.request.use((config) => {
+  const token = useTokenStore.getState().token;
+  if(token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+})
 
 export const getClients = async () => {
   return api.get("/api/clients");
+};
+
+export const createClient = async (data: FormData) => {
+  return api.post("/api/clients", data, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
 };
