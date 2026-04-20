7c344912434a871a4531e800d07cf0fe13a1e03b
Sayak-Mallick <sayakmallickkv@gmail.com>
2026-04-20 11:36:08 +0530

    [TASK] : Events' type defined

---
+++ backend/src/events/events.types.ts
@@ -0,0 +1,11 @@
+export interface Events {
+  _id: number;
+  title: string;
+  description: string;
+  year: number;
+  month: number;
+  location: string;
+  organization: string;
+  category: string;
+  image: string;
+}
